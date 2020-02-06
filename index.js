8da704c340a4731796eaa5ac42972b2e3967f456
Sneha Bhat <snbhtlp@gmail.com>
2020-02-06 23:47:29 +0530

    cleaned up index.js

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const router = require('./config/routes')
  
 const app = express()
 const port = 3015
-// const bodyParser= require('body-parser')
 
 app.use(express.json())
 app.use(cors())
-// app.use(bodyParser.urlencoded({extended: true}))
-// this allows whitelisting etc
+
 setupDB()
 
-// THIS IS ENOUGH to make the images automatically gettable!
 app.use('/uploads', express.static('uploads'))
 
 app.use('/', router)
