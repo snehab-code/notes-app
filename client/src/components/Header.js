fe69290f79a5a876ea4e4ceb7f37ebdf0437983c
Sneha Bhat <nehabphn@gmail.com>
2020-02-08 10:31:25 +0530

    minor alignment changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ function Header(props) {
             <Box display="flex" justifyContent="center" alignItems="center">
                 <Link to="/"><h1 style={{fontSize:24}}>NOTES</h1></Link>
             </Box>
-            <div style={{display: 'flex', justifyContent: "space-evenly", alignItems:"center"}}>
+            <div style={{display: 'flex', justifyContent: "space-evenly", alignItems:"flex-start"}}>
                 {/* <TextField size="small" style={{width:"100%"}} color="secondary" label="Search"/> */}
                 <Link style={{color:"#e0a71f", marginRight:5}} to="/categories">
                     CATEGORIES
