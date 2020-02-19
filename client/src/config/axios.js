42f7fa129f21f091f77cc37535474d05c0859a85
Sneha Bhat <nehabphn@gmail.com>
2020-02-19 18:29:38 +0530

    heroku changes

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: "http://localhost:3015",
+    // baseURL: "http://localhost:3015",
+    baseURL: "/",
     headers: {
         "x-auth": localStorage.getItem('authToken')
     }
