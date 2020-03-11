7592ed080e354762e36b49000e9bdede48e0081e
Sneha Bhat <nehabphn@gmail.com>
2020-03-11 20:54:05 +0530

    deployment tweaks

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     // baseURL: "http://localhost:3015/api",
-    baseURL: "/",
+    baseURL: "/api",
     headers: {
         "x-auth": localStorage.getItem('authToken')
     }
